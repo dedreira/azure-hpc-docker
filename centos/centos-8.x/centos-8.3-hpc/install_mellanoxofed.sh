b45c6692198dfa02bf9db6546231bdcb18c3f82e
Daniel Edreira <dedreira@plainconcepts.com>
2021-04-28 11:29:01 +0200

    disable check kernel while installing mellanox

--- centos/centos-8.x/centos-8.3-hpc/install_mellanoxofed.sh
+++ centos/centos-8.x/centos-8.3-hpc/install_mellanoxofed.sh
@@ -8,8 +8,8 @@ MOFED_FOLDER=$(basename ${MLNX_OFED_DOWNLOAD_URL} .tgz)
 $COMMON_DIR/download_and_verify.sh $MLNX_OFED_DOWNLOAD_URL "dd13ffa54524af9509e3de7288a863aa48ed7016bb3d15446523703020c41715"
 tar zxvf ${TARBALL}
 
-KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
-KERNEL=${KERNEL[-1]}
-yum install -y kernel-devel-${KERNEL}
-./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo
-
+#KERNEL=( $(rpm -q kernel | sed 's/kernel\-//g') )
+#KERNEL=${KERNEL[-1]}
+#yum install -y kernel-devel-${KERNEL}
+#./${MOFED_FOLDER}/mlnxofedinstall --kernel $KERNEL --kernel-sources /usr/src/kernels/${KERNEL} --add-kernel-support --skip-repo
+./${MOFED_FOLDER}/mlnxofedinstall --skip-repo
